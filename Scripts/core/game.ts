44259e7ea2765886989961aa41231220f5404c69
SYJungCC <sjung31@my.centennialcollege.ca>
2019-01-14 17:10:45 -0500

    first commit

---
+++ Scripts/core/game.ts
@@ -0,0 +1,51 @@
+// IIFE - Immediate Invoked Fucntion Expression
+/*
+    Closure
+    Calls an anonympous self-executing function
+    Anything in braces is in a closure. Won't go to global namespace.
+*/
+(function() {
+
+    //Global Game Variables
+    let canvas = document.getElementById("canvas"); // if you change canvas name, you should change this "name"
+    let stage:createjs.Stage; // think unity scene. 
+    let helloLabel: createjs.Text;
+
+    function Init() : void {
+        console.log("Initialization start");
+        Start();
+    }
+
+    
+    function Start() : void
+    {
+        console.log("Starting Application...");
+
+        //Initialize CreateJS
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60 FPS
+        createjs.Ticker.on("tick",Update); // every single tick call update function. 
+        Main();
+    }
+
+    function Update() : void
+    {
+        stage.update();
+
+    }
+
+    function Main() : void
+    {
+        console.log("Game Start...");
+
+        helloLabel = new createjs.Text("Hello World!", "40px Consolate", "#000000");
+        helloLabel.x = 100; 
+        helloLabel.y = 100; 
+
+        stage.addChild(helloLabel);
+    }
+
+
+    window.onload = Init;
+
+})();
