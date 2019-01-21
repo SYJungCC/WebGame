e1fb556f1a1b56378026756290a89f52f7c1c32c
SYJungCC <sjung31@my.centennialcollege.ca>
2019-01-21 18:37:03 -0500

    add button object

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -9,6 +9,7 @@
     var canvas = document.getElementById("canvas"); // if you change canvas name, you should change this "name"
     var stage; // think unity scene. 
     var helloLabel; //I made objects class, that's why the name is objects
+    var clickableButton;
     function Init() {
         console.log("Initialization start");
         Start();
@@ -17,6 +18,7 @@
         console.log("Starting Application...");
         //Initialize CreateJS
         stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20);
         createjs.Ticker.framerate = 60; // 60 FPS
         createjs.Ticker.on("tick", Update); // every single tick call update function. 
         Main();
@@ -29,6 +31,37 @@
         console.log("Game Start...");
         helloLabel = new objects.Label("I wanna go home!", "40px", "Consolas", "#000000", 320, 240, true);
         stage.addChild(helloLabel);
+        clickableButton = new objects.Button("./Assets/Sprites/UIpack/PNG/blue_button00.png", 320, 340);
+        clickableButton.regX = clickableButton.getBounds().width * 0.5;
+        clickableButton.regY = clickableButton.getBounds().height * 0.5;
+        clickableButton.on("click", clickableButtonMouseClick);
+        stage.addChild(clickableButton);
+        /*
+        clickableButton = new createjs.Bitmap("./Assets/Sprites/UIpack/PNG/blue_button00.png");
+        clickableButton.regX = clickableButton.getBounds().width * 0.5;
+        clickableButton.regY = clickableButton.getBounds().height * 0.5;
+        clickableButton.x = 320;
+        clickableButton.y = 340;
+        stage.addChild(clickableButton);
+        
+        clickableButton.on("mouseover",clickableButtonMouseOver);
+        clickableButton.on("mouseout",clickableButtonMouseOut);
+        clickableButton.on("click",clickableButtonMouseClick);
+        */
+    }
+    /*
+        BUTTON EVENT LISTENER FUNCTIONS
+    */
+    function clickableButtonMouseOver() {
+        clickableButton.alpha = 0.7;
+    }
+    function clickableButtonMouseOut() {
+        clickableButton.alpha = 1.0;
+    }
+    function clickableButtonMouseClick() {
+        helloLabel.text = "Clicked!";
+        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
     }
     window.onload = Init;
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -10,7 +10,7 @@
     let canvas = document.getElementById("canvas"); // if you change canvas name, you should change this "name"
     let stage:createjs.Stage; // think unity scene. 
     let helloLabel: objects.Label; //I made objects class, that's why the name is objects
-    let clickableButton: createjs.Bitmap;
+    let clickableButton: objects.Button;
 
     function Init() : void {
         console.log("Initialization start");
@@ -45,11 +45,43 @@
     {
         console.log("Game Start...");
         helloLabel = new objects.Label("I wanna go home!", "40px","Consolas", "#000000", 320, 240, true);
-        clickableButton = new createjs.Bitmap("./Assets/Sprites/UIpack/cliablebutton.png");
         stage.addChild(helloLabel);
+
+        clickableButton = new objects.Button("./Assets/Sprites/UIpack/PNG/blue_button00.png", 320,340);
+        clickableButton.regX = clickableButton.getBounds().width * 0.5;
+        clickableButton.regY = clickableButton.getBounds().height * 0.5;
+        clickableButton.on("click",clickableButtonMouseClick);
+        stage.addChild(clickableButton);
+        
+        /*
+        clickableButton = new createjs.Bitmap("./Assets/Sprites/UIpack/PNG/blue_button00.png");
+        clickableButton.regX = clickableButton.getBounds().width * 0.5;
+        clickableButton.regY = clickableButton.getBounds().height * 0.5;
+        clickableButton.x = 320;
+        clickableButton.y = 340;
+        stage.addChild(clickableButton);
+        
+        clickableButton.on("mouseover",clickableButtonMouseOver);
+        clickableButton.on("mouseout",clickableButtonMouseOut);
+        clickableButton.on("click",clickableButtonMouseClick);
+        */
     }
 
-
+    /* 
+        BUTTON EVENT LISTENER FUNCTIONS
+    */
+    function clickableButtonMouseOver() :void{
+        clickableButton.alpha = 0.7;
+    }
+    function clickableButtonMouseOut() :void{
+        clickableButton.alpha = 1.0;
+    }
+    function clickableButtonMouseClick() :void{
+        helloLabel.text = "Clicked!";
+        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+        
+    }
     window.onload = Init;
 
 })();
